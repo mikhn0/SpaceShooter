ae10d08c271b7b3250adf196699b97e7fe54d283
Viktoria Mikhno <vaporoustransform@gmail.com>
2017-07-07 23:37:02 +0300

    start project 3. Scoring, Finishing and building the game 2. Counting points and displaying the score

---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	[SerializeField]
+	private Rigidbody rb;
+
+	public float tumble;
+
+	void Start() {
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
